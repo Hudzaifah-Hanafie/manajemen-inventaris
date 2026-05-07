2251732d8334eb90f13731623859025bd0e674e8
Hudzaifah-Hanafie <hanafiyaahmad@gmail.com>
2026-05-07 16:07:28 +0700

    Menambahkan fitur edit kategori

---
+++ resources/views/categories/edit.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <div class="card shadow-sm border-0">
+                <div class="card-body p-4">
+                    <div class="d-flex justify-content-between align-items-center mb-4">
+                        <h5 class="card-title m-0">Edit Kategori</h5>
+                        <a href="{{ route('categories.index') }}" class="btn btn-sm btn-secondary">Kembali</a>
+                    </div>
+
+                    <form action="{{ route('categories.update', $category->id) }}" method="post">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nama Kategori</label>
+                            <input type="text" name="name" id="name"
+                                class="form-control @error('name') is-invalid @enderror" placeholder="Misal: Elektronik"
+                                value="{{ old('name', $category->name) }}" required>
+                            @error('name')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary w-100">Simpan Perubahan</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
